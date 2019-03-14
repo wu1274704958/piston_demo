6dab535da0a2aabd9485aacc1db3a369641e95d9
wu1274704958 <1274704958@qq.com>
2019-03-14 18:13:11 +0800

    game of life transparent true

--- src/game_of_life.rs
+++ src/game_of_life.rs
@@ -1,13 +1,15 @@
 extern crate piston_window;
 extern crate glutin_window;
+extern crate piston_demo;
 
 use piston_window::*;
 use glutin_window::GlutinWindow;
+use piston_demo::TransparentWindow::WindowSettings;
 
-const WORLD_W: u32 = 100;
-const WORLD_H: u32 = 100;
+const WORLD_W: u32 = 160;
+const WORLD_H: u32 = 160;
 
-const CELL_W: u32 = 4;
+const CELL_W: u32 = 2;
 
 const fn CELL_W_1_2() -> u32
 {
@@ -32,12 +34,15 @@ fn main() {
         .exit_on_esc(true)
         .samples(32)
         .vsync(true)
-        .decorated(true)
+        .decorated(false)
         .resizable(false)
-        //.transparent(true)
+        .transparent(true)
         .build()
         .unwrap();
     let mut lbd = false;
+    let mut rbd = false;
+    let mut last_mouse_pos = (0.0,0.0);
+
     let mut world = World::new(WORLD_W,WORLD_H);
     world.set_alive(1,0);
     world.set_alive(1,1);
@@ -46,7 +51,7 @@ fn main() {
     while let Some(e) = window.next() {
         if let Event::Loop(Loop::Render(RenderArgs { ext_dt, .. })) = e {
             window.draw_2d(&e, |c: Context, g| {
-                clear([0.0, 0.0, 0.0, 1.0], g);
+                clear([0.0, 0.0, 0.0, 0.5], g);
 
                 for y in 0..WORLD_H{
                     for x in 0..WORLD_W {
@@ -65,6 +70,20 @@ fn main() {
                 let cp = get_cell_pos(x,y);
                 world.set_alive(cp.0,cp.1);
             }
+            if rbd{
+                if last_mouse_pos.0 == 0.0 && last_mouse_pos.1 == 0.0{
+                    last_mouse_pos.0 = x;
+                    last_mouse_pos.1 = y;
+                }else{
+                    let offsetx = x - last_mouse_pos.0;
+                    let offsety = y - last_mouse_pos.1;
+
+                    if offsetx.abs() > 5.0 || offsety.abs() > 5.0{
+                        let now = window.get_position().unwrap();
+                        window.set_position(Position{x:now.x + offsetx as i32, y: now.y + offsety as i32});
+                    }
+                }
+            }
         }
 
 
@@ -81,13 +100,13 @@ fn main() {
                 }
             }
 
-            if let Button::Mouse(MouseButton::Middle) = button {
+            if let Button::Mouse(MouseButton::Right) = button {
                 match state {
                     ButtonState::Press => {
-//                        world.deduction();
+                        rbd = true;
                     }
                     ButtonState::Release => {
-
+                        rbd = false;
                     }
                 }
             }
